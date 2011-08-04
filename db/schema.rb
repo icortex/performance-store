54ab81ac209cb367ad6e2494e693204acf5ad9b9
cortex <cortex@corp.(none)>
2011-08-03 19:49:54 -0500

    reports enhancement v1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110803215619) do
+ActiveRecord::Schema.define(:version => 20110804004435) do
 
   create_table "brands", :force => true do |t|
     t.string   "brand"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(:version => 20110803215619) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "expense_type_id"
+    t.integer  "headquarter_id"
   end
 
   create_table "headquarters", :force => true do |t|
